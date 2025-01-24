423c288b6eb19c51a7ce6ba4e0c0c973ed3038be
vinod <mechvnod@gmail.com>
2025-01-24 14:43:55 +0530

    added under progress message

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -34,6 +34,7 @@ const LandingPage = () => {
           <h1 className="title">Hello, I am Vinod Shetty</h1>
           <p className="subtitle">Welcome to My Complex</p>
         </div>
+        <h3>Great things take time! Our website is under construction, and we’ll be live soon.</h3>
       </div>
     </section>
   );
